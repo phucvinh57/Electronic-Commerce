c82fe008c1db6ca56e91eb08e2f768f008e6b0e6
phucvinh57 <npvinh0507@gmail.com>
2022-11-16 11:33:04 +0700

    add prisma

--- src/Server.ts
+++ src/Server.ts
@@ -10,17 +10,18 @@ import cors from "cors";
 import "@tsed/ajv";
 import "@tsed/swagger";
 import { config } from "./config/index";
-import * as api from "./controllers/api/index";
+import * as apis from "./controllers/apis/index";
 import * as pages from "./controllers/pages/index";
+import { CORS_WHITE_LIST } from "@envs";
 
 @Configuration({
   ...config,
   acceptMimes: ["application/json"],
-  httpPort: process.env.PORT || 8083,
+  httpPort: process.env.PORT || 8081,
   httpsPort: false, // CHANGE
   componentsScan: false,
   mount: {
-    "/api": [...Object.values(api)],
+    "/api": [...Object.values(apis)],
     "/": [...Object.values(pages)]
   },
   swagger: [
@@ -30,7 +31,10 @@ import * as pages from "./controllers/pages/index";
     }
   ],
   middlewares: [
-    cors(),
+    cors({
+      origin: CORS_WHITE_LIST,
+      credentials: true
+    }),
     cookieParser(),
     compress({}),
     methodOverride(),
--- src/config/envs/index.ts
+++ src/config/envs/index.ts
@@ -4,4 +4,7 @@ export const envs = {
   ...process.env,
   ...dotenv.config().parsed
 };
+
+export const CORS_WHITE_LIST = (envs.CORS_WHITE_LIST as string).split(',')
+
 export const isProduction = process.env.NODE_ENV === "production";
