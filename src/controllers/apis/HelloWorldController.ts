ec27d6f0eb522fcdfbe93313d5cbe45bb747a96b
phucvinh57 <npvinh0507@gmail.com>
2022-11-16 11:54:15 +0700

    test husky

--- src/controllers/apis/HelloWorldController.ts
+++ src/controllers/apis/HelloWorldController.ts
@@ -1,16 +1,14 @@
 import { Controller, Inject } from "@tsed/di";
 import { HelloWorldsRepository } from "@tsed/prisma";
-import { Get, Post } from "@tsed/schema";
+import { Get } from "@tsed/schema";
 
 @Controller("/hello-world")
 export class HelloWorldController {
-  @Inject()
-  private helloRepo: HelloWorldsRepository
+    @Inject()
+    private helloRepo: HelloWorldsRepository;
 
-  @Get("/")
-  async get() {
-    let a;
-    return this.helloRepo.findMany();
-  }
-
+    @Get("/")
+    async get() {
+        return this.helloRepo.findMany();
+    }
 }
